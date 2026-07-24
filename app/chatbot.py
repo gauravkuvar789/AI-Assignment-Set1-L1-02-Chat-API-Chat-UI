5f6168e7160a85dd88646add845a956e9e5c6298
gauravkuvar789 <gauravkuvar789@gmail.com>
2026-07-24 17:51:11 +0530

    new

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,20 +1,16 @@
 import google.generativeai as genai
 
-from app.config import GEMINI_API_KEY
-from app.config import MODEL_NAME
+from app.config import GEMINI_API_KEY, MODEL_NAME
 
 genai.configure(api_key=GEMINI_API_KEY)
 
 model = genai.GenerativeModel(MODEL_NAME)
 
-
 def ask_llm(history):
-
     prompt = ""
 
     for msg in history:
-
-        prompt += f"{msg['role']} : {msg['content']}\n"
+        prompt += f"{msg['role']}: {msg['content']}\n"
 
     response = model.generate_content(prompt)
 
